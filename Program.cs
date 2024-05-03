c76d9f5d8887797d05b91d05310f8ee17a0240a7
duyd486 <duyd486@gmail.com>
2024-05-04 01:43:52 +0700

    add

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap ten cua ban: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Xin chao " + name);
+    }
+}
